e2be03784fa3fe9397d991d357bea05e1006eedf
Andrzej Surowiec <emeryth@gmail.com>
2020-03-08 20:16:30 +0100

    Update screenshot script.

--- gfx/screenshot.sh
+++ gfx/screenshot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#dump_image screen.raw 0x20004eb0 15488
-echo "dump_image $PWD/screen.raw 0x20005010 15488" | telnet localhost 4444
+ADDR="$(readelf -s ../armgcc/_build/application.out | grep lcd_buffer | awk '{print $2}')"
+echo "dump_image $PWD/screen.raw 0x$ADDR 15488" | telnet localhost 4444
 sleep 1
 convert -depth 1 -size 176x176 RGBA:screen.raw screen.png
+rm screen.raw
